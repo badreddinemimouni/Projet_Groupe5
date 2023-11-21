a1ddd50e9b846c06a2d4aa6e17b0c70e92e8d6a3
BADHTW <badr_mim@outlook.fr>
2023-11-21 11:58:19 +0100

    dazerfez

--- src/Views/header.php
+++ src/Views/header.php
@@ -9,8 +9,8 @@ use Tp\Project\App\Dispatcher;
         <ul class="">
             <li class=""><a class="nav-link text-light" href="<?php Dispatcher::Dispatch('IndexController', 'index'); ?>">Accueil</a></li>
             <li class=""><a class="nav-link text-light" href="<?php Dispatcher::Dispatch('ConnexionController', 'connexion'); ?>">Se connecter</a></li>
-            <li class=""><a class="nav-link text-light" href="<?php Dispatcher::Dispatch('SecurityController', 'inscription'); ?>">S'enregistrer</a></li>
-            <li class=""><a class="nav-link text-light" href="<?php Dispatcher::Dispatch('SecurityController', 'dexonnexion'); ?>">Se déconnecter</a></li>
+            <li class=""><a class="nav-link text-light" href="<?php Dispatcher::Dispatch('usersController', 'createUser'); ?>">S'enregistrer</a></li>
+            <li class=""><a class="nav-link text-light" href="<?php Dispatcher::Dispatch('SecurityController', 'deconnexion'); ?>">Se déconnecter</a></li>
         </ul>
     </nav>
 </header>
