ed6848a2aed6a55cc4a6ecd334da2ba4b795e3c9
Cyril <cyril.personne@outlook.fr>
2023-11-22 09:38:20 +0100

    push

--- src/Views/projects.php
+++ src/Views/projects.php
@@ -1,6 +1,5 @@
 <main>
     <?php
-    include_once(__DIR__ . './header.php');
     foreach ($projects as $project) {
         echo "Titre :" . $project->getName();
     }
--- src/Views/tasks.php
+++ src/Views/tasks.php
@@ -1,6 +1,5 @@
 <main>
     <?php
-    include_once(__DIR__ . './header.php');
     foreach ($tasks as $task) {
         echo "Titre :" . $task->getTitle();
     }
